93a48fc06c6238510ab6b51ac5b3c46fa8717a9d
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-06-08 10:35:34 +0200

    Add VectorAxis enums

---
+++ Assets/BetterCommons/Runtime/Enums/VectorAxes/Vector4Axis.cs
@@ -0,0 +1,10 @@
+﻿namespace Better.Commons.Runtime.Enums
+{
+    public enum Vector4Axis
+    {
+        X = 0,
+        Y = 1,
+        Z = 2,
+        W = 4,
+    }
+}
--- Assets/BetterCommons/Runtime/Utility/Vector4Utility.cs
+++ Assets/BetterCommons/Runtime/Utility/Vector4Utility.cs
@@ -5,51 +5,58 @@ using UnityEngine;
 
 namespace Better.Commons.Runtime.Utility
 {
-    public struct Vector2Utility
+    public struct Vector4Utility
     {
-        public static bool Approximately(Vector2 current, Vector2 other)
+        public static bool Approximately(Vector4 current, Vector4 other)
         {
-            return Mathf.Approximately(current.x, other.x) &&
-                   Mathf.Approximately(current.y, other.y);
+            return Mathf.Approximately(current.x, other.x)
+                   && Mathf.Approximately(current.y, other.y)
+                   && Mathf.Approximately(current.z, other.z)
+                   && Mathf.Approximately(current.w, other.w);
         }
 
-        public static Vector2 MiddlePoint(Vector2 start, Vector2 end)
+        public static Vector4 MiddlePoint(Vector4 start, Vector4 end)
         {
             var t = start + end;
             return t / 2;
         }
 
-        public static Vector2 MiddlePoint(Vector2 start, Vector2 end, Vector2 offset)
+        public static Vector4 MiddlePoint(Vector4 start, Vector4 end, Vector4 offset)
         {
             var middlePoint = MiddlePoint(start, end);
             return middlePoint + offset;
         }
 
-        public static Vector2 SlerpUnclamped(Vector2 a, Vector2 b, float t)
+        public static Vector4 SlerpUnclamped(Vector4 a, Vector4 b, float t)
         {
-            var dot = Vector2.Dot(a.normalized, b.normalized);
+            a.Normalize();
+            b.Normalize();
+
+            var dot = Vector4.Dot(a, b);
             dot = Mathf.Clamp(dot, -1.0f, 1.0f);
+
             var theta = Mathf.Acos(dot) * t;
-            var relativeVector = (b - a * dot).normalized;
+            var relativeVector = b - a * dot;
+            relativeVector.Normalize();
 
             return a * Mathf.Cos(theta) + relativeVector * Mathf.Sin(theta);
         }
 
-        public static Vector2 Slerp(Vector2 a, Vector2 b, float t)
+        public static Vector4 Slerp(Vector4 a, Vector4 b, float t)
         {
             t = Mathf.Clamp01(t);
             return SlerpUnclamped(a, b, t);
         }
 
-        public static Vector2 AxesInverseLerp(Vector2 a, Vector2 b, Vector2 value)
+        public static Vector4 AxesInverseLerp(Vector4 a, Vector4 b, Vector4 value)
         {
-            return new Vector2(
+            return new Vector4(
                 Mathf.InverseLerp(a.x, b.x, value.x),
                 Mathf.InverseLerp(a.y, b.y, value.y)
             );
         }
 
-        public static float InverseLerp(Vector2 a, Vector2 b, Vector2 value)
+        public static float InverseLerp(Vector4 a, Vector4 b, Vector4 value)
         {
             if (a == b)
             {
@@ -59,30 +66,30 @@ namespace Better.Commons.Runtime.Utility
             var ab = b - a;
             var av = value - a;
 
-            var result = Vector2.Dot(av, ab) / Vector2.Dot(ab, ab);
+            var result = Vector4.Dot(av, ab) / Vector4.Dot(ab, ab);
             return Mathf.Clamp01(result);
         }
 
-        public static Vector2 Direction(Vector2 from, Vector2 to)
+        public static Vector4 Direction(Vector4 from, Vector4 to)
         {
             var difference = to - from;
             return difference.normalized;
         }
 
-        public static float SqrDistanceTo(Vector2 from, Vector2 to)
+        public static float SqrDistanceTo(Vector4 from, Vector4 to)
         {
             var difference = to - from;
             return difference.sqrMagnitude;
         }
 
-        public static Vector2 Abs(Vector2 source)
+        public static Vector4 Abs(Vector4 source)
         {
             source.x = Mathf.Abs(source.x);
             source.y = Mathf.Abs(source.y);
             return source;
         }
 
-        public static Vector2 ApplyAxes(Vector2 target, Vector2 source, IEnumerable<Vector2Axis> axes)
+        public static Vector4 ApplyAxes(Vector4 target, Vector4 source, IEnumerable<Vector4Axis> axes)
         {
             foreach (var axis in axes)
             {
