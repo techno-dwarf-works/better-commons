d5cc5f3c0ef51ac562fddf1c35904012616d61bc
uurha <uurha@users.noreply.github.com>
2024-07-30 21:30:37 +0000

    Merge pull request #31 from techno-dwarf-works/feature/refactoring
    Version 0.0.28

--- Runtime/Extensions/FuncExtensions.cs
+++ Runtime/Extensions/FuncExtensions.cs
@@ -9,11 +9,12 @@ namespace Better.Commons.Runtime.Extensions
 
         #region SafeInvoke`16
 
-        public static TResult SafeInvoke<TResult>(this Func<TResult> self, bool logException = DefaultLogException)
+        public static bool TryInvoke<TResult>(this Func<TResult> self, out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke();
+                result = self.Invoke();
+                return true;
             }
             catch (Exception exception)
             {
@@ -22,18 +23,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T, TResult>(
+        public static bool TryInvoke<T, TResult>(
             this Func<T, TResult> self,
             T arg,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg);
+                result = self.Invoke(arg);
+                return true;
             }
             catch (Exception exception)
             {
@@ -42,18 +45,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, TResult>(
+        public static bool TryInvoke<T1, T2, TResult>(
             this Func<T1, T2, TResult> self,
             T1 arg1, T2 arg2,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2);
+                result = self.Invoke(arg1, arg2);
+                return true;
             }
             catch (Exception exception)
             {
@@ -62,18 +67,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, TResult>(
+        public static bool TryInvoke<T1, T2, T3, TResult>(
             this Func<T1, T2, T3, TResult> self,
             T1 arg1, T2 arg2, T3 arg3,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3);
+                result = self.Invoke(arg1, arg2, arg3);
+                return true;
             }
             catch (Exception exception)
             {
@@ -82,18 +89,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, TResult>(
             this Func<T1, T2, T3, T4, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4);
+                result = self.Invoke(arg1, arg2, arg3, arg4);
+                return true;
             }
             catch (Exception exception)
             {
@@ -102,18 +111,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, TResult>(
             this Func<T1, T2, T3, T4, T5, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5);
+                return true;
             }
             catch (Exception exception)
             {
@@ -122,18 +133,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6);
+                return true;
             }
             catch (Exception exception)
             {
@@ -142,18 +155,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+                return true;
             }
             catch (Exception exception)
             {
@@ -162,18 +177,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+                return true;
             }
             catch (Exception exception)
             {
@@ -182,18 +199,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+                return true;
             }
             catch (Exception exception)
             {
@@ -202,18 +221,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+                return true;
             }
             catch (Exception exception)
             {
@@ -222,18 +243,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
+                return true;
             }
             catch (Exception exception)
             {
@@ -242,18 +265,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12);
+                return true;
             }
             catch (Exception exception)
             {
@@ -262,18 +287,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13);
+                return true;
             }
             catch (Exception exception)
             {
@@ -282,18 +309,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14);
+                return true;
             }
             catch (Exception exception)
             {
@@ -302,18 +331,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15);
+                return true;
             }
             catch (Exception exception)
             {
@@ -322,18 +353,20 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
-        public static TResult SafeInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(
+        public static bool TryInvoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(
             this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> self,
             T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16,
-            bool logException = DefaultLogException)
+            out TResult result, bool logException = DefaultLogException)
         {
             try
             {
-                return self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16);
+                result = self.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16);
+                return true;
             }
             catch (Exception exception)
             {
@@ -342,7 +375,8 @@ namespace Better.Commons.Runtime.Extensions
                     DebugUtility.LogException(exception);
                 }
 
-                return default;
+                result = default;
+                return false;
             }
         }
 
