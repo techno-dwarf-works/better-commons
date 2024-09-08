e61490c1433ebc0306982ee374cd18c1ab9afc08
uurha <uurha.dev@gmail.com>
2024-09-08 17:34:00 +0200

    Rework GetPropertyContainers

---
+++ Assets/BetterCommons/Editor/Helpers/PropertyParent.cs
@@ -0,0 +1,17 @@
+﻿namespace Better.Commons.EditorAddons.Helpers
+{
+    public class PropertyParent
+    {
+        public object ParentInstance { get; }
+
+        public string FieldName { get; }
+        public int ElementIndex { get; }
+
+        public PropertyParent(object parentInstance, string fieldName, int elementIndex)
+        {
+            ParentInstance = parentInstance;
+            FieldName = fieldName;
+            ElementIndex = elementIndex;
+        }
+    }
+}
